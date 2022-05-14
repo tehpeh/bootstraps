448beabfb9da906d531ab2f8c96c052df7211a51
Tim Preston <tim@timpreston.net>
2022-05-15 09:00:32 +1000

    Update macos bootstrap for 2022

--- macos-bootstrap.sh
+++ macos-bootstrap.sh
@@ -4,27 +4,25 @@ set -e
 # Download: curl -LO https://bit.ly/macos-bootstrap
 
 # Symlink dotfiles
-# TODO: path below should use path of this file?
-# TODO: remove . from source files and prefix to target file
-# TODO: add sublime packages/user files
-# TODO: make a git repo for all this, including dot/config files
-FILES=./macosx/.*
-for f in $FILES; do
-  if [[ $f =~ .DS_Store$ ]]; then
-    continue
-  fi
-  if [[ -f $f ]]; then
-    echo "Linking $f to ~"
-    ln -sf $f ~
-  fi
-done
-
-source ~/.bash_profile
+# TODO: redo/rethink this section, can pull these files from backup
+# IDEA: remove . from source files and prefix to target file
+# IDEA: add sublime packages/user files
+# IDEA: make a separate git repo or folder for all this, including dot/config files
+# FILES=./macosx/.*
+# for f in $FILES; do
+#   if [[ $f =~ .DS_Store$ ]]; then
+#     continue
+#   fi
+#   if [[ -f $f ]]; then
+#     echo "Linking $f to ~"
+#     ln -sf $f ~
+#   fi
+# done
 
 # Copy .ssh/config
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-cp -n dotfiles/macosx/.ssh/config ~/.ssh/config
+# mkdir -p ~/.ssh
+# chmod 700 ~/.ssh
+# cp -n dotfiles/macosx/.ssh/config ~/.ssh/config
 
 # Install Xcode Command Line Tools
 xcode-select --install
@@ -33,49 +31,73 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Mac apps
-
-brew cask install \
+brew install --cask \
   alfred \
+  arq \
   caffeine \
   dash \
   firefox \
   google-chrome \
-  hyperswitch \
+  iterm2 \
+  keybase \
+  menumeters \
   omnidisksweeper \
-  rowanj-gitx \
+  plexamp \
+  resilio-sync \
   sizeup \
-  sketch \
-  sketch-toolbox \
   slack \
   sublime-text \
-  vagrant \
-  vienna \
   virtualbox \
   vlc \
-  menumeters \
+  zerotier-one \
+
+# Optional: install old/unused/broken apps
+# brew install --cask \
+#   1password \
+#   hyperswitch \
+#   sketch \
+#   sketch-toolbox \
+#   rowanj-gitx \
+#   vagrant \
+#   vienna \
 
 # Install command line apps
-
 brew install \
   ack \
   direnv \
-  docker \
-  docker-machine \
-  docker-compose \
   fish \
+  gnupg \
   go \
   git \
-  imagemagick #--disable-openmp --build-from-source \
+  htop \
+  imagemagick \
   node \
   nvim \
   pass \
   pass-otp \
+  pinentry-mac \
   proctools \
-  qt@5.5 \
   rbenv \
   ruby-build \
+  tig \
   tmux \
   wget \
+  yarn \
+
+# NOTE: imagemagick may need flags: --disable-openmp --build-from-source
+
+# Install browserpass native client
+brew tap amar1729/formulae
+brew install browserpass
+PREFIX='/usr/local/opt/browserpass' make hosts-chrome-user -f '/usr/local/opt/browserpass/lib/browserpass/Makefile'
+PREFIX='/usr/local/opt/browserpass' make hosts-firefox-user -f '/usr/local/opt/browserpass/lib/browserpass/Makefile'
+
+# Optional: install old/unused command line apps
+# brew install \
+#   docker \
+#   docker-machine \
+#   docker-compose \
+#   qt@5.5 \
 
 echo "========================================"
 echo "             setup required!            "
@@ -110,3 +132,7 @@ echo "Wifi Signal"
 echo "The Unarchiver"
 echo "Pixelmator"
 echo "ForkLift"
+
+echo "Download 1password 7 from https://app-updates.agilebits.com/download/OPM7"
+echo "Restore from Arq backup before launching apps"
+echo "Check placeholders exist in selective sync folders before launching Resilio Sync, otherwise files will be deleted"
